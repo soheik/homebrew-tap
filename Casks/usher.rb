5d4318428ccd120919686c33fe3f4349a56bcb50
soheik <soheik@gmail.com>
2026-03-21 13:24:55 +0900

    first commit

---
+++ Casks/usher.rb
@@ -0,0 +1,11 @@
+cask "usher" do
+  version "1.0.0"
+  sha256 "9d453fe7ce1b760b6393c9c8dfa1df48fec35c9a8bfcb35ad22b08edd8f50d82"
+
+  url "https://github.com/soheik/Usher/releases/download/v#{version}/Usher-#{version}.zip"
+  name "Usher"
+  desc "Set default apps for file extensions on macOS"
+  homepage "https://github.com/soheik/Usher"
+
+  app "Usher.app"
+end
---
+++ README.md
@@ -0,0 +1,17 @@
+# Homebrew Tap
+
+## Installation
+
+```bash
+brew tap soheik/tap
+```
+
+## Available Formulae
+
+### Usher
+
+Set default apps for file extensions on macOS.
+
+```bash
+brew install soheik/Usher
+```
