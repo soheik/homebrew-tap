7005cc73290baa930253e31bc4fcea9b418dd1b8
soheik <soheik@gmail.com>
2026-03-24 09:31:58 +0900

    update cask

--- Casks/usher.rb
+++ Casks/usher.rb
@@ -6,6 +6,10 @@ cask "usher" do
   name "Usher"
   desc "Set default apps for file extensions on macOS"
   homepage "https://github.com/soheik/Usher"
-
   app "Usher.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+      args: ["-dr", "com.apple.quarantine", "#{appdir}/Usher.app"]
+  end
 end
